8049aa28ee62f14bb0e5546b2ef54776a1df83b4
Nikhilkumar06 <nikhiltupakula255@gmail.com>
2023-11-08 23:42:05 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+
+from django.http import HttpResponse
+def gajjala(request):
+    return HttpResponse('Hlo Sir Iam Gajjala from Washington DC')
